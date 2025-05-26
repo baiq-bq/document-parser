fadfe59f38fc665987bbbe5076058baa8f56be70
Alberto Valero Gómez <alberto.valero@bqeducacion.cc>
2025-05-26 16:36:24 +0200

    missing files

---
+++ mod.ts
@@ -0,0 +1,7 @@
+export { extractPDFContent } from "./pdf-extractor/mod.ts";
+export type { PDFExtractionResult } from "./pdf-extractor/mod.ts";
+export { extractEPUBContent } from "./epub-extractor/mod.ts";
+export type { EPUBExtractionResult } from "./epub-extractor/mod.ts";
+export { extractDOCXContent } from "./docx-extractor/mod.ts";
+export type { DOCXExtractionResult } from "./docx-extractor/mod.ts";
+export type { PageContent } from "./types.ts";
---
+++ types.ts
@@ -0,0 +1,4 @@
+export type PageContent = {
+  images: string[];
+  paragraphs: string[];
+};
